130b43974977c8665541bf20334c5715c5d6fc2a
yosef brandwein <yosefdbrandwein@gmail.com>
2019-12-26 14:43:01 -0500

    Project Done

---
+++ Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalaryCalculater
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public string Role { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+        public double Bonus { get => CalculateBonus(); }
+
+        private double CalculateBonus()
+        {
+            switch (this.Role)
+            {
+                case "Executive":
+                    return this.Salary * .20;
+                case "Supervisor":
+                    return this.Salary * .10;
+                case "Manager":
+                    return this.Salary * .08;
+                case "Employee":
+                    return this.Salary * .05;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,86 @@ namespace SalaryCalculater
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+            Employee Roth = new Employee()
+            {
+                Role = "Executive",
+                Title = "Mr",
+                FirstName = "Moshe",
+                LastName = "Roth",
+                Salary = 250000
+            };
+            Employee Stark = new Employee()
+            {
+                Role = "Supervisor",
+                Title = "Mr",
+                FirstName = "Chaim",
+                LastName = "Stark",
+                Salary = 150000
+            };
+            Employee Spira = new Employee()
+            {
+                Role = "Supervisor",
+                Title = "Mr",
+                FirstName = "Moshe",
+                LastName = "Spira",
+                Salary = 160000
+            };
+            Employee Gold = new Employee()
+            {
+                Role = "Manager",
+                Title = "Mr",
+                FirstName = "Yehuda",
+                LastName = "Gold",
+                Salary = 120000
+            };
+            Employee Weiss = new Employee()
+            {
+                Role = "Manager",
+                Title = "Mrs",
+                FirstName = "Miriam",
+                LastName = "Weiss",
+                Salary = 120000
+            };
+            Employee Green = new Employee()
+            {
+                Role = "Employee",
+                Title = "Mr",
+                FirstName = "Shlomo",
+                LastName = "Green",
+                Salary = 65000
+            };
+            Employee Silver = new Employee()
+            {
+                Role = "Employee",
+                Title = "Mrs",
+                FirstName = "Ester",
+                LastName = "Silver",
+                Salary = 75000
+            };
+            Employee Weiser = new Employee()
+            {
+                Role = "Employee",
+                Title = "Mr",
+                FirstName = "Yakov",
+                LastName = "Weiser",
+                Salary = 70000
+            };
+            List<Employee> Employees = new List<Employee>();
+            Employees.Add(Roth);
+            Employees.Add(Stark);
+            Employees.Add(Spira);
+            Employees.Add(Gold);
+            Employees.Add(Weiss);
+            Employees.Add(Green);
+            Employees.Add(Silver);
+            Employees.Add(Weiser);
+
+            List<Employee> EmployeesSorted = Employees.OrderBy(e => e.Bonus).ToList();
+            list.ItemsSource = EmployeesSorted;
         }
+
+        
     }
+
 }
