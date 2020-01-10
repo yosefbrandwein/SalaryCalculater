e961b0f801591f360b98b203514f04d85fbac855
yosef brandwein <yosefdbrandwein@gmail.com>
2020-01-10 12:54:15 -0500

    put data in file

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,79 +25,26 @@ namespace SalaryCalculater
         {
             InitializeComponent();
             this.DataContext = this;
-            Employee Roth = new Employee()
-            {
-                Role = "Executive",
-                Title = "Mr",
-                FirstName = "Moshe",
-                LastName = "Roth",
-                Salary = 250000
-            };
-            Employee Stark = new Employee()
-            {
-                Role = "Supervisor",
-                Title = "Mr",
-                FirstName = "Chaim",
-                LastName = "Stark",
-                Salary = 150000
-            };
-            Employee Spira = new Employee()
-            {
-                Role = "Supervisor",
-                Title = "Mr",
-                FirstName = "Moshe",
-                LastName = "Spira",
-                Salary = 160000
-            };
-            Employee Gold = new Employee()
-            {
-                Role = "Manager",
-                Title = "Mr",
-                FirstName = "Yehuda",
-                LastName = "Gold",
-                Salary = 120000
-            };
-            Employee Weiss = new Employee()
-            {
-                Role = "Manager",
-                Title = "Mrs",
-                FirstName = "Miriam",
-                LastName = "Weiss",
-                Salary = 120000
-            };
-            Employee Green = new Employee()
-            {
-                Role = "Employee",
-                Title = "Mr",
-                FirstName = "Shlomo",
-                LastName = "Green",
-                Salary = 65000
-            };
-            Employee Silver = new Employee()
-            {
-                Role = "Employee",
-                Title = "Mrs",
-                FirstName = "Ester",
-                LastName = "Silver",
-                Salary = 75000
-            };
-            Employee Weiser = new Employee()
-            {
-                Role = "Employee",
-                Title = "Mr",
-                FirstName = "Yakov",
-                LastName = "Weiser",
-                Salary = 70000
-            };
+            
             List<Employee> Employees = new List<Employee>();
-            Employees.Add(Roth);
-            Employees.Add(Stark);
-            Employees.Add(Spira);
-            Employees.Add(Gold);
-            Employees.Add(Weiss);
-            Employees.Add(Green);
-            Employees.Add(Silver);
-            Employees.Add(Weiser);
+            using (var reader = new StreamReader(@"C:\Users\User\source\repos\Salary.csv"))
+            {
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    var employee = new Employee()
+                    {
+                        Role = values[0],
+                        Title = values[1],
+                        FirstName = values[2],
+                        LastName = values[3],
+                        Salary = Convert.ToInt32(values[4])
+                    };
+                    Employees.Add(employee);
+                }
+            }
 
             List<Employee> EmployeesSorted = Employees.OrderBy(e => e.Bonus).ToList();
             list.ItemsSource = EmployeesSorted;
